cfa72b9b5323930a0767f7da989b5a5d00f994d8
Piotr Martyński <p.martynski@gmail.com>
2018-12-13 23:23:44 +0100

    Suppress env info logs in CI

--- scripts/envinfo.ps1
+++ scripts/envinfo.ps1
@@ -1,2 +1,5 @@
-Get-ChildItem env: | Where-Object { $_.Name.StartsWith("APPVEYOR") -or $_.Name.StartsWith("CI") }
-dotnet --version
+if($env:SUPPRESS_ENV_INFO -ne $true) {
+	Get-ChildItem env: | Where-Object { $_.Name.StartsWith("APPVEYOR") -or $_.Name.StartsWith("CI") }
+	Write-Output "dotnet --version"
+	dotnet --version
+}
