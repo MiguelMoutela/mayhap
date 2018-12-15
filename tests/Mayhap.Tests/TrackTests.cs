5c614a2c5eaf5db7276fa1f685f3a38b8b36aec8
Piotr Martyński <p.martynski@gmail.com>
2018-12-15 13:11:14 +0100

    Track unit tests added

--- tests/Mayhap.Tests/TrackTests.cs
+++ tests/Mayhap.Tests/TrackTests.cs
@@ -1,7 +1,86 @@
-﻿namespace Mayhap.Tests
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Mayhap.Tests
 {
     public class TrackTests
     {
-        
+        public static IEnumerable<object[]> ContinuationTheory => new[]
+        {
+            new object[] { "fault".Fail<int>(), "onSuccess".Success(), "fault".Fail<string>() }, 
+            new object[] { 100.Success(), "onSuccess".Success(), "onSuccess".Success() },
+            new object[] { 100.Success(), "nextFault".Fail<string>(), "nextFault".Fail<string>() },
+        };
+
+        [Theory]
+        [MemberData(nameof(ContinuationTheory))]
+        public void GivenSyncFuncMaybeAndExistingMaybe_WhenContinuationCalledOnExistingMaybe_ThenShouldReturnMaybeWithProperties(
+            Maybe<int> previousMaybe,
+            Maybe<string> continuationResult,
+            Maybe<string> expectedMaybe)
+        {
+            // given
+            Maybe<string> Continuation(int _) => continuationResult;
+
+            // when
+            var actual = previousMaybe.Continue(Continuation);
+
+            // then
+            actual.Should().Be(expectedMaybe);
+        }
+
+        [Theory]
+        [MemberData(nameof(ContinuationTheory))]
+        public async Task GivenAsyncFuncMaybeAndExistingMaybe_WhenContinuationCalledOnExistingMaybe_ThenShouldReturnMaybeWithProperties(
+            Maybe<int> previousMaybe,
+            Maybe<string> continuationResult,
+            Maybe<string> expectedMaybe)
+        {
+            // given
+            Task<Maybe<string>> Continuation(int _) => Task.FromResult(continuationResult);
+
+            // when
+            var actual = await previousMaybe.Continue(Continuation);
+
+            // then
+            actual.Should().Be(expectedMaybe);
+        }
+
+        [Theory]
+        [MemberData(nameof(ContinuationTheory))]
+        public void GivenSyncContinue_WhenCalled_ThenReturnSameMaybeAsReturnAndAsOutParameter(
+            Maybe<int> previousMaybe,
+            Maybe<string> continuationResult,
+            Maybe<string> __)
+        {
+            // given
+            Maybe<string> Continuation(int _) => continuationResult;
+
+            // when
+            var returnedMaybe = previousMaybe.Continue(Continuation, out var outParamMaybe);
+
+            // then
+            returnedMaybe.Should().Be(outParamMaybe);
+        }
+
+        [Theory]
+        [MemberData(nameof(ContinuationTheory))]
+        public async Task GivenAsyncContinue_WhenCalled_ThenReturnSameMaybeAsReturnAndAsOutParameter(
+            Maybe<int> previousMaybe,
+            Maybe<string> continuationResult,
+            Maybe<string> __)
+        {
+            // given
+            Task<Maybe<string>> Continuation(int _) => Task.FromResult(continuationResult);
+
+            // when
+            var returnedMaybe = await previousMaybe.Continue(Continuation, out var outParamMaybeTask);
+            var outParamMaybe = await outParamMaybeTask;
+
+            // then
+            returnedMaybe.Should().Be(outParamMaybe);
+        }
     }
 }
